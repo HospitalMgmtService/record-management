14df6c6c0b42920023846acb2e30918649775e62
Phong Vo <vhphong@gmail.com>
2024-08-09 22:48:51 -0500

    testUploadFile_Success

---
+++ src/main/java/com/pnk/record_management/controller/StorageController.java
@@ -0,0 +1,51 @@
+package com.pnk.record_management.controller;
+
+import com.pnk.record_management.service.StorageServiceImpl;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("files")
+@RequiredArgsConstructor // injected by Constructor, no longer need of @Autowire
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class StorageController {
+
+    StorageServiceImpl storageService;
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(storageService.uploadFile(file), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = storageService.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; file=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        storageService.deleteFile(fileName);
+        return ResponseEntity
+                .ok()
+                .body("File was deleted successfully.");
+    }
+}
